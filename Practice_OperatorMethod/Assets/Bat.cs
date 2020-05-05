c061e74992725f1d21d29207cf436273ca84e403
kuso1327 <63327433+kuso1327@users.noreply.github.com>
2020-05-05 14:30:41 +0800

    加上顏色

--- Practice_OperatorMethod/Assets/Bat.cs
+++ Practice_OperatorMethod/Assets/Bat.cs
@@ -7,7 +7,49 @@ public class Bat : MonoBehaviour
     public int atk;
     public int heal;
 
+    #region 老師的作法
+    /*
+    [Header("顯示訊息")]
+    public Text message;
+    [Header("史萊姆")]
+    public Slime slime;
+
+    private float Hp = 150;
+    private float Atk = 50;
+    private float cure = 20;
+
+
+    /// <summary>
+    /// 攻擊
+    /// </summary>
+    public void Attack()
+    {
+        slime.Damage(Atk);
+    }
+    /// <summary>
+    /// 受傷
+    /// </summary>
+    /// <param name="damage">要接受的傷害值</param>
     
+    public void Damage(float damage)
+    {
+        Hp -= damage;//Hp=Hp-damage
+        message.text = "<color=#243224>"+gameObject.name +"</color>"+ "-受到傷害" + damage;
+        message.text += "\n" + gameObject.name + "-血量剩下" + damage;
+    }
+    /// <summary>
+    /// 治癒
+    /// </summary>
+    public void Cure()
+    {
+        Hp += cure;
+        message.text = gameObject.name + "-受到治癒" + cure;
+        message.text += "\n" + gameObject.name + "-血量剩下" + cure;
+    }
+    */
+    #endregion
+
+
 
 
 
--- Practice_OperatorMethod/Assets/Operation.cs
+++ Practice_OperatorMethod/Assets/Operation.cs
@@ -15,15 +15,15 @@ public class Operation : MonoBehaviour
     public void bathurt()
     {
         bat.hp=bat.hp-slime.atk;
-        result.text = "蝙蝠-受到傷害：" +slime.atk + "\n" 
-                    + "蝙蝠-血量剩下：" + bat.hp;
+        result.text = "<color=#2A352A>"+ "蝙蝠" + "</color>" + "-受到傷害：" +slime.atk + "\n" 
+                    + "<color=#2A352A>"+"蝙蝠" + "</color>" + "-血量剩下：" + bat.hp;
     }
 
     public void batcure()
     {
         bat.hp = bat.hp + bat.heal;
-        result.text = "蝙蝠-受到治癒：" + bat.heal+ "\n" 
-                    + "蝙蝠-血量剩下：" + bat.hp;
+        result.text = "<color=#2A352A>"+"蝙蝠"+ "</color>" +"- 受到治癒：" + bat.heal+ "\n" 
+                    + "<color=#2A352A>"+"蝙蝠"+ "</color>" +"- 血量剩下：" + bat.hp;
     }
 
     public void slimeatk()
@@ -34,15 +34,15 @@ public class Operation : MonoBehaviour
     public void slimehurt()
     {
         slime.hp=slime.hp-bat.atk;
-        result.text = "史萊姆-受到攻擊：" + bat.atk+ "\n" 
-                    + "史萊姆-血量剩下：" + slime.hp;
+        result.text = "<color=#848D13>"+"史萊姆"+"</color>"+"-受到攻擊：" + bat.atk+ "\n" 
+                    + "<color=#848D13>"+"史萊姆"+ "</color>" + "-血量剩下：" + slime.hp;
     }
 
     public void slimecure()
     {
         slime.hp = slime.hp +slime.heal;
-        result.text = "史萊姆-受到治癒：" + slime.heal + "\n" 
-                    + "史萊姆-血量剩下：" + slime.hp;
+        result.text = "<color=#848D13>"+"史萊姆"+ "</color>" + "-受到治癒：" + slime.heal + "\n" 
+                    + "<color=#848D13>"+"史萊姆"+ "</color>" + "-血量剩下：" + slime.hp;
     }
 
 
